3b247682c2df39527b6edd62bc429dc42c129f91
ayush-kumar774 <ayushkumar1234@protonmail.com>
2020-12-06 12:45:21 +0530

    usual commit

---
+++ Codeforces/1415 - A. Prison Break.cpp
@@ -0,0 +1,62 @@
+// In the name of Aadi Shakti
+// We are nothing and you are everything
+// Jai Mata Di
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fast ios_base::sync_with_stdio(false) ; cin.tie(0) ; cout.tie(0) ;
+#define endl '\n'
+#define sz(v)	( (int) ( (v).size() ) )
+#define all(v)	( (v).begin() ) , ( (v).end() )
+#define allr(v)	( (v).rbegin() ) , ((v).rend())
+#define vector (vector<int>)
+#define vectorInt vector<int>
+#define vectorStr vector<string>
+#define vectorDo vector<double>
+#define pb push_back
+const int MAX_N = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+
+string decimalToBinary(int n) 
+{ 
+        string s = bitset<64> (n).to_string(); 
+        const auto loc1 = s.find('1'); 
+        
+        if(loc1 != string::npos) 
+                return s.substr(loc1); 
+        
+        return "0"; 
+} 
+
+void solve()
+{
+        int n , m , r , c ;
+        cin >> n >> m >> r >> c ;
+        cout << max( r - 1 , n - r ) + max ( c - 1 , m - c ) << endl ;
+}
+
+int32_t main()
+{
+        #ifndef ONLINE_JUDGE
+	        freopen("input.txt", "r", stdin);
+	        freopen("output.txt", "w", stdout);
+        #endif
+        fast ;
+        int testcases = 1 ;
+        cin >> testcases ;
+        while(testcases--)
+        {
+                solve() ;
+        }
+        return 0;
+}
+/*  CODED BY:-
+         ___________________________________
+        |                       ___         |
+        |  /\   /\  \ /  |  |  |___   |__|  |
+        | /~~\ /~~\  |   |__|   ___|  |  |  |
+        |___________________________________|
+*/
