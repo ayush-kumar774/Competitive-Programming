fd6c2d7475ec7f76cd5f1cff1a41c34768c47acf
ayush-kumar774 <ayushkumar1234@protonmail.com>
2021-01-27 19:50:15 +0530

    usual commit

--- Leetcode/350. Intersection of Two Arrays II.cpp
+++ Leetcode/350. Intersection of Two Arrays II.cpp
@@ -1,74 +1,81 @@
 #include <bits/stdc++.h>
-using namespace std ;
+using namespace std;
 #define endl "\n"
-#define fast ios_base::sync_with_stdio(false) ; cin.tie(0) ; cout.tie(0) ;
+#define fast                              \
+	ios_base::sync_with_stdio(false); \
+	cin.tie(0);                       \
+	cout.tie(0);
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> intersection ;
-        int n = nums1.size();
-        int m = nums2.size();
-        if ( n < m )
-		{       for (auto x : nums1)
-				{
-					for(int i = 0 ; i < m ; i++ )
-					{
-						if ( nums2[i] == x )
-						{
-							
-							nums2[i] = INT_MAX ;
-							intersection.push_back(x) ;
-							break ;
-						}
-					}
-				}
-		}
-		else 
+	vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
+	{
+		vector<int> intersection;
+		int n = nums1.size();
+		int m = nums2.size();
+		if (n < m)
 		{
-			 for (auto x : nums2)
+			for (auto x : nums1)
+			{
+				for (int i = 0; i < m; i++)
 				{
-					for(int i = 0 ; i < n ; i++ )
+					if (nums2[i] == x)
 					{
-						if ( nums1[i] == x )
-						{
-							
-							nums1[i] = INT_MAX ;
-							intersection.push_back(x) ;
-							break ;
-						}
+
+						nums2[i] = INT_MAX;
+						intersection.push_back(x);
+						break;
 					}
 				}
+			}
 		}
-        return intersection ;
-    }
+		else
+		{
+			for (auto x : nums2)
+			{
+				for (int i = 0; i < n; i++)
+				{
+					if (nums1[i] == x)
+					{
+
+						nums1[i] = INT_MAX;
+						intersection.push_back(x);
+						break;
+					}
+				}
+			}
+		}
+		return intersection;
+	}
 };
 
 int main()
 {
-    fast ;
-    
-    int n ;
-    cin >> n ;
-    
-    vector<int> nums1(n) ;
-    for(auto &x : nums1) cin >> x ;
-    
-    int m ;
-    cin >> m ;
-    
-    vector<int> nums2(m) , ans ;
-    for(auto &x : nums2) cin >> x ;
+	fast;
 
-    Solution s ;
-    ans = s.intersect(nums1, nums2) ;
+	int n;
+	cin >> n;
 
-    for (auto x : ans)
-    {
-		cout << x << " " ; 
- 	}
- 	cout << endl ;
+	vector<int> nums1(n);
+	for (auto &x : nums1)
+		cin >> x;
 
-    return  0 ;
+	int m;
+	cin >> m;
+
+	vector<int> nums2(m), ans;
+	for (auto &x : nums2)
+		cin >> x;
+
+	Solution s;
+	ans = s.intersect(nums1, nums2);
+
+	for (auto x : ans)
+	{
+		cout << x << " ";
+	}
+	cout << endl;
+
+	return 0;
 }
-
